5c9c8158a87a7571636cb1814586e0a13a701542
Pedro Ruiz <otaku14.pr@gmail.com>
2021-10-27 21:24:50 -0500

    Registro y Login mejorados, validacion en la bd , actualizancion de datos en la bd

--- chuntels/bdChuntels/forms.py
+++ chuntels/bdChuntels/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 
 from bdChuntels.models import carrear
+import datetime 
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 
 class RegisterForm(forms.Form):
     
@@ -8,6 +13,20 @@ class RegisterForm(forms.Form):
     email = forms.EmailField(label='Email')
     password = forms.CharField(label='Password', widget=forms.PasswordInput())
     nickname = forms.CharField(label='Nickname', max_length=100)
-    #fotoPerfilUsuario = forms.ImageField(label='Foto de perfil')
-    edad = forms.IntegerField(label='Edad')
+    fotoPerfilUsuario = forms.ImageField(label='Foto de perfil', widget=forms.FileInput() , required=False)
+    nacimiento = forms.DateField(label='Fecha Nacimiento' , widget=DateInput())
     carrear = forms.ModelChoiceField(label='Carrera', queryset=carrear.objects.all())
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+
+
+class EditForm(forms.Form):
+    newUsername = forms.CharField(label='Username', max_length=100 , required=False)
+    newEmail = forms.EmailField(label='Email' , required=False)
+    newNickname = forms.CharField(label='Nickname', max_length=100 , required=False)
+    newPassword = forms.CharField(label='Password', widget=forms.PasswordInput() , required=False)
+    newfotoPerfilUsuario = forms.ImageField(label='Foto de perfil', widget=forms.FileInput() , required=False)
+    newCarrear = forms.ModelChoiceField(label='Carrera', queryset=carrear.objects.all() , required=False)
