3ae2e49a068f4f02253faf824ad46f183ba7426c
Pedro Ruiz <otaku14.pr@gmail.com>
2021-11-10 22:41:39 -0500

    Agregar datos por register

--- chuntels/bdChuntels/admin.py
+++ chuntels/bdChuntels/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.db import models
-from bdChuntels.models import carrear, User
+from bdChuntels.models import *
 # Register your models here.
 
 admin.site.register(carrear)
 admin.site.register(User)
+admin.site.register(typePost)
+admin.site.register(TypeNotification)
+admin.site.register(Notification)
+admin.site.register(Post)
+admin.site.register(Coments)
+admin.site.register(Follow)
+admin.site.register(Like)
+admin.site.register(Friend)
+admin.site.register(stateMessage)
+admin.site.register(Chat)
+admin.site.register(Message)
+admin.site.register(Work)
